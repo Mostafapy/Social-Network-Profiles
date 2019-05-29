51469088a99bc3c7b3e9f6b3ab2b72b23e5043c1
mostafa <mmham@MostafaHesham.com>
2019-05-29 17:00:37 +0200

    adding validations on the users/ route

---
+++ validations/usersRequest.validation.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+const userRequestSchema = Joi.object()
+  .required()
+  .keys({
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2 })
+      .required(),
+    password: Joi.string()
+      .min(6)
+      .required(),
+  });
+
+const userRequestValidation = (req, res, next) => {
+  const validatedBody = Joi.validate(req.body, userRequestSchema);
+
+  if (validatedBody.error !== null) {
+    const error = validatedBody.error.details[0];
+
+    const errStr = `${error.message}, (error path): ${error.path.join('.')}`;
+
+    return res.status(422).json({
+      err: errStr,
+      msg: 'Input validation error.',
+      data: null,
+    });
+  }
+
+  return next();
+};
+
+module.exports = userRequestValidation;
