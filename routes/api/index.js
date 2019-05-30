3d409553116972001c8faa6b50288851705ce891
mostafa <mmham@MostafaHesham.com>
2019-05-30 14:29:22 +0200

    adding authorization route to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 // Routers
 const usersRouter = require('./users');
-// const authRouter = require('./auth');
+const authRouter = require('./auth');
 // const profileRouter = require('./profile');
 // const postsRouter = require('./posts');
 
 // Routes
-// router.use('/auth', authRouter);
+router.use('/api', authRouter);
 // router.use('/profile', profileRouter);
 // router.use('/posts', postsRouter);
 router.use('/api', usersRouter);
