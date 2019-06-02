e0ff81c9de8fb70eba34a4511ca451c5369f4dbd
mostafa <mmham@MostafaHesham.com>
2019-06-02 07:52:45 +0200

    enhance routers in the index folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-
+const apiRouters = require('express').Router();
 // Routers
 const usersRouter = require('./users');
 const authRouter = require('./auth');
-// const profileRouter = require('./profile');
+const profileRouter = require('./profile');
 // const postsRouter = require('./posts');
 
 // Routes
-router.use('/api', authRouter);
-// router.use('/profile', profileRouter);
-// router.use('/posts', postsRouter);
-router.use('/api', usersRouter);
+router.use('/api', apiRouters);
+apiRouters.use('/auth', authRouter);
+apiRouters.use('/profile', profileRouter);
+// apiRouters.use('/posts', postsRouter);
+apiRouters.use('/users', usersRouter);
 
 module.exports = router;
