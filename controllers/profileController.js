b0450fd918fd9d13ac1c23b16e99d9a2247f749e
mostafa <mmham@MostafaHesham.com>
2019-06-08 11:15:41 +0200

    add userProfileCreatorOrUpdater controller in profileController

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -28,4 +28,61 @@ const userProfileRetriever = async (req, res) => {
   }
 };
 
-module.exports = { userProfileRetriever };
+const userProfileCreatorOrUpdater = async (req, res) => {
+  const {
+    company,
+    website,
+    location,
+    bio,
+    skills,
+    status,
+    githubusername,
+    youtube,
+    facebook,
+    instagram,
+    linkedin,
+    twitter,
+  } = req.body;
+  try {
+    const profileOBJ = {};
+
+    profileOBJ.user = req.user.id;
+
+    if (company) profileOBJ.company = company;
+    if (youtube) profileOBJ.youtube = youtube;
+    if (facebook) profileOBJ.facebook = facebook;
+    if (instagram) profileOBJ.instagram = instagram;
+    if (twitter) profileOBJ.twitter = twitter;
+    if (linkedin) profileOBJ.linkedin = linkedin;
+    if (githubusername) profileOBJ.githubusername = githubusername;
+    if (website) profileOBJ.website = website;
+    if (bio) profileOBJ.bio = bio;
+    if (location) profileOBJ.location = location;
+
+    profileOBJ.skills = skills.split(',').map(skill => skill.trim());
+    profileOBJ.status = status;
+
+    const actionDone = await profileCRUDLogic.createOrUpdateProfile(
+      profileOBJ.user,
+      profileOBJ,
+    ); // created //updated
+
+    if (actionDone === 'created')
+      return res
+        .status(200)
+        .json({ err: null, msg: `One User Profile Created`, data: null });
+
+    return res
+      .status(200)
+      .json({ err: null, msg: `One User Profile Updated`, data: null });
+  } catch (err) {
+    logger.error('@userProfileCreatorOrUpdater() [error: %0]', err.message);
+
+    return res.status(500).json({
+      err: null,
+      msg: 'Cannot Create Or Update User Profile',
+      data: null,
+    });
+  }
+};
+module.exports = { userProfileRetriever, userProfileCreatorOrUpdater };
