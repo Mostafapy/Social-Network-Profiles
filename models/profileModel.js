553ccf3869c8d4a6ad36ed19e83736682675f0c3
mostafa <mmham@MostafaHesham.com>
2019-06-14 11:32:32 +0200

    fix versionKey property in userModel and profileModel

--- models/profileModel.js
+++ models/profileModel.js
@@ -29,89 +29,91 @@ const educationSchema = new mongoose.Schema({
     type: String,
   },
 });
-const profileSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  },
-  company: {
-    type: String,
-  },
-  website: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  skills: {
-    type: [String],
-    required: true,
-  },
-  bio: {
-    type: String,
-  },
-  githubusername: {
-    type: String,
-  },
-  experience: [
-    {
-      _id: false,
-      title: {
+const profileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    company: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    githubusername: {
+      type: String,
+    },
+    experience: [
+      {
+        _id: false,
+        title: {
+          type: String,
+          required: true,
+        },
+        company: {
+          type: String,
+          required: true,
+        },
+        location: {
+          type: String,
+        },
+        from: {
+          type: Date,
+          required: true,
+        },
+        to: {
+          type: Date,
+        },
+        current: {
+          type: Boolean,
+          default: false,
+        },
+        description: {
+          type: String,
+        },
+      },
+    ],
+    education: {
+      type: [educationSchema],
+    },
+    social: {
+      youtube: {
         type: String,
-        required: true,
       },
-      company: {
-        type: String,
-        required: true,
-      },
-      location: {
+      twitter: {
         type: String,
       },
-      from: {
-        type: Date,
-        required: true,
+      facebook: {
+        type: String,
       },
-      to: {
-        type: Date,
+      instagram: {
+        type: String,
       },
-      current: {
-        type: Boolean,
-        default: false,
-      },
-      description: {
+      linkedin: {
         type: String,
       },
     },
-  ],
-  education: {
-    type: [educationSchema],
-  },
-  social: {
-    youtube: {
-      type: String,
-    },
-    twitter: {
-      type: String,
-    },
-    facebook: {
-      type: String,
-    },
-    instagram: {
-      type: String,
-    },
-    linkedin: {
-      type: String,
+    date: {
+      type: Date,
+      default: Date.now,
     },
   },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  versionKey: false,
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('profile', profileSchema);
