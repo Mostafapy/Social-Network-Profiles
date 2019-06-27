ab26a12f8aeacc2c6e3440b96ac2b15c881ac62a
mostafa <mmham@MostafaHesham.com>
2019-06-27 14:33:46 +0200

    make versionKey false in the post schema database

--- models/postsModel.js
+++ models/postsModel.js
@@ -1,56 +1,59 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-  },
-  avatar: {
-    type: String,
-  },
-  likes: [
-    {
-      _id: false,
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-      },
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
     },
-  ],
-  comments: [
-    {
-      _id: false,
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-      },
-      text: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-      },
-      avatar: {
-        type: String,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      },
+    text: {
+      type: String,
+      required: true,
     },
-  ],
-  date: {
-    type: Date,
-    default: Date.now,
+    name: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    likes: [
+      {
+        _id: false,
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'user',
+        },
+      },
+    ],
+    comments: [
+      {
+        _id: false,
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'user',
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: String,
+        },
+        avatar: {
+          type: String,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('post', postSchema);
