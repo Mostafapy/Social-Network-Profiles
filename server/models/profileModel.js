73d8f122dadead9da42de2a5251839993650cfc6
mostafa <mmham@MostafaHesham.com>
2019-08-14 17:35:38 +0200

    server: adding handle attribute to profileModel

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -34,6 +34,11 @@ const profileSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'user',
     },
+    handle: {
+      type: String,
+      required: true,
+      max: 40,
+    },
     company: {
       type: String,
     },
