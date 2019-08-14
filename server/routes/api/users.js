00d8cb2a337122a9a595c19936b9a3e7fef6d202
mostafa <mmham@MostafaHesham.com>
2019-08-14 13:26:26 +0200

    server: fix some routes in user routes and add /current get method route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const userRegistrationValidation = require('../../validations/usersRequest.validation');
 
+const { isAuthorized } = require('../../middlewares/authorization');
 const userCtrl = require('../../controllers/usersController');
 
 const router = express.Router();
-// [POST] api/users
+// [POST] api/users/register
 // @desc  Register user
 // @access Public
-router.post('/', userRegistrationValidation, userCtrl.userRegistration);
+router.post('/register', userRegistrationValidation, userCtrl.userRegistration);
 
+// [GET] api/users/current
+// @desc  get current user
+// @access private
+router.get('/current', isAuthorized, userCtrl.retrieveCurrentUser);
 module.exports = router;
